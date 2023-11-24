3778c005fa699bf75aa0dd4f42e58be35c331c38
Sahin <sahin.kasap@nanodems.com>
2023-11-24 17:18:05 +0300

    initial

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ClassLibrary1
+{
+public class Class1
+{
+public static void LogTest()
+{
+        if (!EventLog.SourceExists(System.AppDomain.CurrentDomain.FriendlyName))
+            EventLog.CreateEventSource(System.AppDomain.CurrentDomain.FriendlyName, "Application");
+}
+}
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClassLibrary1;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.LogTest();
+        }
+    }
+}
