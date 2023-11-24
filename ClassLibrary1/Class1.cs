6bc13c3f7e0b874e47bae8a0a2c00a6201a5ed15
Sahin <sahin.kasap@nanodems.com>
2023-11-24 17:19:57 +0300

    prettifying

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -3,12 +3,12 @@ using System.Diagnostics;
 
 namespace ClassLibrary1
 {
-public class Class1
-{
-public static void LogTest()
-{
-        if (!EventLog.SourceExists(System.AppDomain.CurrentDomain.FriendlyName))
-            EventLog.CreateEventSource(System.AppDomain.CurrentDomain.FriendlyName, "Application");
-}
-}
+    public class Class1
+    {
+        public static void LogTest()
+        {
+            if (!EventLog.SourceExists(System.AppDomain.CurrentDomain.FriendlyName))
+                EventLog.CreateEventSource(System.AppDomain.CurrentDomain.FriendlyName, "Application");
+        }
+    }
 }
